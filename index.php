afedbe97b3af7b0aee886e6e31f3c5e537fe28c4
Katre Laur <katrelaur@gmail.com>
2023-09-28 19:29:07 +0300

    Add simple number and string examples

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$num = 1;
+echo($num);
+echo "hello world";
+var_dump($num);
+var_dump($num);
+$num = 1.4;
+var_dump($num); 
+$num = $num + 1;
+$num += 1; 
+$num++;
+$num--;
+$num -= 2;
+$num *= 3;
+$num /= 2;
+$num = 5;
+$num %= 2;
+var_dump($num);
+$string = "sõna" ;
+$string = 'hello' . $string;
+$string .= '!!!!';
+var_dump($string);
+$hello = 'word is: $string';
+var_dump($hello);
+
